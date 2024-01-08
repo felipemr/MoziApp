82e007863f11fdd8bef005682094738b8e9152a2
felipemr <felipemr94@outlook.com>
2024-01-08 14:26:34 -0300

    Created and added the init project

---
+++ Mozi/Mozi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mozi
+//
+//  Created by Felipe Marques on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mozi/Mozi/MoziApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoziApp.swift
+//  Mozi
+//
+//  Created by Felipe Marques on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MoziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
